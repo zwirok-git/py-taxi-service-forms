52b52f250c7b38d6acc6926f891b34d790ef4bf1
Oleksii Prokopenko <oleksijprokopenko@gmail.com>
2026-05-17 17:49:07 +0200

    Update urls.py

--- taxi/urls.py
+++ taxi/urls.py
@@ -23,33 +23,33 @@ urlpatterns = [
         name="manufacturer-list",
     ),
     path(
-        "manufacturer/<int:pk>/update/",
+        "manufacturers/<int:pk>/update/",
         ManufacturerUpdateView.as_view(),
         name="manufacturer-update"
     ),
     path(
-        "manufacturer/<int:pk>/delete/",
+        "manufacturers/<int:pk>/delete/",
         ManufacturerDeleteView.as_view(),
         name="manufacturer-delete"
     ),
     path(
-        "manufacturer/create/",
+        "manufacturers/create/",
         ManufacturerCreateView.as_view(),
         name="manufacturer-create"
     ),
     path("cars/", CarListView.as_view(), name="car-list"),
     path(
-        "car/<int:pk>/update/",
+        "cars/<int:pk>/update/",
         CarUpdateView.as_view(),
         name="car-update"
     ),
     path(
-        "car/<int:pk>/delete/",
+        "cars/<int:pk>/delete/",
         CarDeleteView.as_view(),
         name="car-delete"
     ),
     path(
-        "car/create/",
+        "cars/create/",
         CarCreateView.as_view(),
         name="car-create"
     ),
